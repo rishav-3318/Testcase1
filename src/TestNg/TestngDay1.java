02c6ecd5edfd8fbebb252a51ec2c680396af370f
rishav <rishav3318@gmail.com>
2022-06-02 13:48:40 +0530

    my first commit

---
+++ src/TestNg/TestngDay1.java
@@ -0,0 +1,16 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class TestngDay1 {
+
+    @Test
+    public void Demo(){
+        System.out.println("Hello");
+
+    }
+    @Test
+    public void SecondTest(){
+        System.out.println("Bye");
+    }
+}
